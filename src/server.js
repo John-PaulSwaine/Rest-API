d08184489361c05eb59f42c82cf16f2fca52199f
John-Paul Swaine <179924473+John-PaulSwaine@users.noreply.github.com>
2024-10-21 11:54:27 +0100

    added cors

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
+const cors = require('cors')
 const express = require('express')
 const app = express()
 require('dotenv').config()
-const port = process.env.PORT || 5001
 app.use(express.json())
+app.use(cors())
 
 
 
 
-
+const port = process.env.PORT || 5001
 app.get('/health', (req,res)=>{res.status(200).send('API is healthy')})
 app.listen(port, ()=>{console.log(`server listening on port ${port}`)})
