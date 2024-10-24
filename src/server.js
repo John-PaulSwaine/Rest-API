13b63be4bf17430a6033b5056f30e2afe9ddb095
John-Paul Swaine <179924473+John-PaulSwaine@users.noreply.github.com>
2024-10-24 14:50:05 +0100

    pulled Game model and gameRouter into the server.js

--- src/server.js
+++ src/server.js
@@ -3,17 +3,20 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const User = require('./db/models/userModel')
+const Game = require('./db/models/gameModel')
+const gameRouter = require('./db/routes/gameRoutes')
 const userRouter = require('./db/routes/userRoutes')
 app.use(express.json())
 
 const syncTables = () => {
     User.sync({alter:true})
+    Game.sync({alter:true})
 };
 syncTables()
 
 
 app.use(userRouter)
-
+app.use(gameRouter)
 app.use(cors())
 
 const port = process.env.PORT || 5001
